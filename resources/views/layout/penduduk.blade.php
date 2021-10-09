42702552a9cbe2f3fa76ae19eba290ecdd60a072
khosyrobbin <khosybin@gmail.com>
2021-10-09 23:58:03 +0700

    Create table

--- resources/views/layout/penduduk.blade.php
+++ resources/views/layout/penduduk.blade.php
@@ -2,7 +2,39 @@
 
 @section('content')
     <div class="content">
-        <div class="container-fluid"><h1>Halaman Penduduk</h1></div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title ">PENDUDUK</h4>
+                        {{-- <p class="card-category"> Here is a subtitle for this table</p> --}}
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class=" text-primary">
+                                  <th>
+                                    NIK
+                                  </th>
+                                  <th>
+                                    Nama
+                                  </th>
+                                  <th>
+                                    Jenis Kelamin
+                                  </th>
+                                  <th>
+                                    No Hp
+                                  </th>
+                                  <th>
+                                    Alamat
+                                  </th>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
--- resources/views/layout/wilayah.blade.php
+++ resources/views/layout/wilayah.blade.php
@@ -1,8 +1,43 @@
 @extends('layout.template')
 
 @section('content')
-    <div class="content">
-        <div class="container-fluid"><h1>Halaman Wilayah</h1></div>
+<div class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header card-header-primary">
+                    <h4 class="card-title ">WILAYAH</h4>
+                    {{-- <p class="card-category"> Here is a subtitle for this table</p> --}}
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class=" text-primary">
+                              <th>
+                                ID
+                              </th>
+                              <th>
+                                Nama Daerah
+                              </th>
+                              <th>
+                                Alamat
+                              </th>
+                              <th>
+                                Kelurahan
+                              </th>
+                              <th>
+                                Kecamatan
+                              </th>
+                              <th>
+                                  Alasan
+                              </th>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 
 @endsection
